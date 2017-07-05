ec1507e793b266c81191276bab957ee7bb3caade
Nick Ocampo <nick@wehatenick.com>
2017-07-05 16:03:59 -0700

    set up angular material

--- client/app/app.js
+++ client/app/app.js
@@ -1,27 +1,26 @@
 'use strict';
 
 import angular from 'angular';
-// import ngAnimate from 'angular-animate';
+import ngAnimate from 'angular-animate';
 import ngCookies from 'angular-cookies';
 import ngResource from 'angular-resource';
 import ngSanitize from 'angular-sanitize';
-
 import uiRouter from 'angular-ui-router';
+// import ngMessages from 'angular-messages';
+import ngMaterial from 'angular-material';
+import {routeConfig} from './app.config';
 
-import {
-  routeConfig
-} from './app.config';
-
-import navbar from '../components/navbar/navbar.component';
-import footer from '../components/footer/footer.component';
 import main from './main/main.component';
 import constants from './app.constants';
 import util from '../components/util/util.module';
+// import header from '../components/header/header.component';
+import footer from '../components/footer/footer.component';
 
 import './app.scss';
 
-angular.module('tetrationRoiApp', [ngCookies, ngResource, ngSanitize, uiRouter, navbar, footer,
-  main, constants, util
+angular.module('tetrationRoiApp', [ngCookies, ngResource, ngSanitize, 
+	uiRouter, ngAnimate, ngMaterial, constants, util,
+	main, footer
 ])
   .config(routeConfig);
 
