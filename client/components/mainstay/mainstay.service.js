3c1a3a4e9c029710979a68d072a7f37767ffb31c
Nick Ocampo <nick@wehatenick.com>
2017-07-27 01:34:27 -0700

    set up get field shadow values

---
+++ client/components/mainstay/mainstay.service.js
@@ -0,0 +1,14 @@
+
+// export {msUpdateInputs};
+ // function msUpdateOutputs() {
+ //   let shadowOutput = document.getElementById('shadow-output');
+ //   if (shadowOutput) {
+ //     let scope = angular.element(shadowOutput).scope();
+ //     if (scope && scope.$ctrl) {
+ //       scope.$ctrl.results.forEach((section) => {
+ //         section.score.value = document.getElementById(section.score.dbEntity).value;
+ //         section.average.value = document.getElementById(section.average.dbEntity).value;
+ //       });
+ //     }
+ //   }
+ // }
